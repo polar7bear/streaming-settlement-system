bdca16cc625736b28d90653358bc63b93c65ca01
polar7bear <sons1998@naver.com>
2024-10-19 10:14:23 +0900

    project initialized

---
+++ member-service/src/test/java/com/streaming/settlement/system/memberservice/MemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.settlement.system.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ streaming-ad-service/src/main/java/com/streaming/settlement/system/streamingadservice/StreamingAdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.settlement.system.streamingadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingAdServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingAdServiceApplication.class, args);
+    }
+
+}
