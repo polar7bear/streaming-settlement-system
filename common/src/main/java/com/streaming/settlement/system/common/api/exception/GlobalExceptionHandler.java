0a10a38bfaf2ed26b872e1cc4f70c3b65e2c63d7
polar7bear <sons1998@naver.com>
2024-10-21 11:13:13 +0900

    Feat: 전역 예외처리 클래스 작성 및 JpaAuditing 작성 / common

---
+++ common/src/main/java/com/streaming/settlement/system/common/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.streaming.settlement.system.common.api.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
