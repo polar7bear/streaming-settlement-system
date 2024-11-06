9e07a1443c45b5ba091cc508a7ef03acbbd39b5f
polar7bear <sons1998@naver.com>
2024-11-06 16:02:29 +0900

    Fix: 광고 시청기록 테이블 스키마 수정으로 인한 로직수정

--- streaming-ad-service/src/main/java/com/streaming/settlement/system/streamingadservice/repository/AdViewLogRepository.java
+++ streaming-ad-service/src/main/java/com/streaming/settlement/system/streamingadservice/repository/AdViewLogRepository.java
@@ -2,8 +2,9 @@ package com.streaming.settlement.system.streamingadservice.repository;
 
 import com.streaming.settlement.system.streamingadservice.domain.entity.AdViewLog;
 import com.streaming.settlement.system.streamingadservice.domain.entity.Advertisement;
+import com.streaming.settlement.system.streamingadservice.domain.entity.StreamingAdMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdViewLogRepository extends JpaRepository<AdViewLog, Long> {
-    boolean existsByAdvertisementAndIpAddressAndMemberId(Advertisement ad, String ipAddress, Long memberId);
+    boolean existsByMappingAndIpAddressAndMemberId(StreamingAdMapping mapping, String ipAddress, Long memberId);
 }
--- streaming-ad-service/src/main/java/com/streaming/settlement/system/streamingadservice/service/AdvertisementService.java
+++ streaming-ad-service/src/main/java/com/streaming/settlement/system/streamingadservice/service/AdvertisementService.java
@@ -34,18 +34,18 @@ public class AdvertisementService {
 
             // 예를들어 10분(600초)짜리 영상에 300초가 첫광고 시청 시점임 getadPlayTime은 스트리밍 등록되는 순간 광고도 자동적으로 insert된다. (일단 둘다 더미데이터로 추가하는게 맞음)
             // 그리고 사용자의 재생시간이 등록된 영상의 광고 재생타임인 300초를 넘기게되면 광고 시청 처리 하면된다.
-            if (ad.getAdPlayTime() <= currentTime) {
+            if (mapping.getPlayTime() <= currentTime) {
                 // TODO: 수익은 배치 처리할 때 알아보도록 하자.
                 if (!isAbuse) {
-                    ad.incrementCount();
-                    advertisementRepository.save(ad);
+                    mapping.incrementCount();
+                    streamingAdMappingRepository.save(mapping);
                 }
-                boolean hasViewed = adViewLogRepository.existsByAdvertisementAndIpAddressAndMemberId(ad, ipAddress, memberId);
+                boolean hasViewed = adViewLogRepository.existsByMappingAndIpAddressAndMemberId(mapping, ipAddress, memberId);
                 if (!hasViewed) {
                     AdViewLog adViewLogEntity = AdViewLog.builder()
                             .ipAddress(ipAddress)
                             .viewedAt(LocalDateTime.now())
-                            .advertisement(ad)
+                            .mapping(mapping)
                             .memberId(memberId) //스트리밍 엔티티의 memberId는 동영상 등록한 creator의 회원 고유번호임. 당연하게도 엔드포인트에서 요청으로 들어온 회원의 id로 저장해야한다.
                             .build();
 
