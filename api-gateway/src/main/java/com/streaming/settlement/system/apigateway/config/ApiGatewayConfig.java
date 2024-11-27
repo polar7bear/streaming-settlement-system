0abbe3731724b17320bf5b1de9b71da1711a6ec5
polar7bear <sons1998@naver.com>
2024-11-28 00:06:46 +0900

    Feat: 전체 라우팅 설정

--- api-gateway/src/main/java/com/streaming/settlement/system/apigateway/config/ApiGatewayConfig.java
+++ api-gateway/src/main/java/com/streaming/settlement/system/apigateway/config/ApiGatewayConfig.java
@@ -1,18 +1,49 @@
 package com.streaming.settlement.system.apigateway.config;
 
+import com.streaming.settlement.system.apigateway.filter.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class ApiGatewayConfig {
 
+    private final RouteLocatorBuilder builder;
+    private final JwtFilter jwtFilter;
+
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    public RouteLocator routeLocator() {
+        JwtFilter.Config memberConfig = new JwtFilter.Config();
+        memberConfig.setExcludePaths(List.of(
+                "/members/auth/sign-up",
+                "/members/auth/sign-in",
+                "/oauth/**"
+        ));
+
+        JwtFilter.Config settlementConfig = new JwtFilter.Config();
+        settlementConfig.setExcludePaths(List.of(
+                "/statistics/**"
+        ));
+
+
         return builder.routes()
+                .route(p -> p.path("/members/auth/**")
+                        .filters(f -> f.filter(jwtFilter.apply(memberConfig)))
+                        .uri("lb://member-service"))
+
                 .route(p -> p.path("/streams/**")
                         .uri("lb://streaming-ad-service"))
+
+                .route(p -> p.path("/statistics/**", "/settlements/**")
+                        .filters(f -> f.filter(jwtFilter.apply(settlementConfig)))
+                        .uri("lb://settlement-service"))
 
                 .build();
     }
